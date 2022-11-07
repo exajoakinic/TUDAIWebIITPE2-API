fab10ff493a0eaea0670e5c5f36fb0a9ab90c431
Joaquin Iglesias Coqueti <jiglesiascoqueti@alumnos.exa.unicen.edu.ar>
2022-11-06 21:11:58 -0300

    reemplazo operador ternario por operador comparacion  ??

--- app/views/api.view.php
+++ app/views/api.view.php
@@ -26,6 +26,6 @@ class ApiView {
       404 => "Not found",
       500 => "Internal Server Error"
     );
-    return (isset($status[$code])) ? $status[$code] : $status[500];
+    return $status[$code] ?? $status[500];
   }
 }
